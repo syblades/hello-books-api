fdaa4ad50b50928afb4fa7d7b35200d373b5291c
syblades <86990570+syblades@users.noreply.github.com>
2021-10-24 18:48:10 -0700

    modified "/<book_id>"  code to support PUT requests

--- app/routes.py
+++ app/routes.py
@@ -1,82 +1,47 @@
-# routes.py = file where we define our endpoints
 
-from flask import Blueprint
-# We need to import our dependencies. Python supports comma-separated importing.
-# # jsonify is a Flask utility function that turns its argument into JSON. 
-# use jsonify to turn a list of book dictionaries into a Response object.
-from flask import Blueprint, jsonify 
-
-
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
-
-books = [
-    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-] 
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["GET"])
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return jsonify(books_response)
-
-
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book_id = int(book_id)
-    for book in books:
-        if book.id == book_id:
-            return {
+    if request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
                 "id": book.id,
                 "title": book.title,
                 "description": book.description
-            }
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
 
+        db.session.add(new_book)
+        db.session.commit()
+        return make_response(f"Book {new_book.title} successfully created", 201)
 
+@books_bp.route("/<book_id>", methods=["GET", "PUT"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
 
-# "hello_world" = first argument is a string to be used to identify Blueprint from Flask server logs (in terminal)
-# "__name__" = second argument is almost always "__name__" which blueprint uses to figure out aspects of routing.
-hello_world_bp = Blueprint("hello_world", __name__)
+    if request.method == "GET":
+        return {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        }
+    elif request.method == "PUT":
+        form_data = request.get_json()
 
-# "@blueprint_name.route(...)" = decorator transforms the function that follows into an endpoint
-# use ".route()" instance method from Blueprint instance
-@hello_world_bp.route("/hello-world", methods=["GET"]) 
+        book.title = form_data["title"]
+        book.description = form_data["description"]
 
-# function will execute whenever a request that matches the decorator is received
-def say_hello_world():
-    # must define a response body to return. 
-    my_beautiful_response_body = "Hello, World!"
-    # For each endpoint, we must return the HTTP response.
-    return my_beautiful_response_body
+        db.session.commit()
+        return make_response(f"Book #{book.id} successfully updated")
 
-@hello_world_bp.route("/hello/JSON", methods=["GET"]) 
-def say_hello_json():
-    return {
-        "name": "Harry Potter",
-        "message": "Wingardium Leviosa",
-        "hobbies": ["Smashing Demintours", "Dating Ming Lee", "Being a bad-ass"]
-    }
-
-
-@hello_world_bp.route("/broken-endpoint", methods=["GET"])
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
