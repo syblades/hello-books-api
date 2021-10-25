8e02a3684effcb8b9fa508e7552eff71eaa352e9
syblades <86990570+syblades@users.noreply.github.com>
2021-10-24 18:48:39 -0700

    updated db configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
+# postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development
 # package file that contains alot of the start up code for app
-
 def create_app(test_config=None):
     app = Flask(__name__)
-
-    # importing hello_world_bp into this module so we may use it in the next line
-    from .routes import hello_world_bp 
-    # use app's pre-defined function "register_blueprint()"to register hello_world_bp Blueprint
-    app.register_blueprint(hello_world_bp) 
+    
+    # db config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    
+    # connecting db and migrate to Flask app
+    # initializing db oobject
+    db.init_app(app)
+    # telling migrate how to connect to the db
+    migrate.init_app(app, db)
+    from app.models.book import Book
 
     from .routes import books_bp
     app.register_blueprint(books_bp)
-    
-    return app
 
+		
+    return app
